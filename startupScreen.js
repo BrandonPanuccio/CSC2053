dc2ba8fd6a77ef512dfc06fdbf5e9410a4edcac6
BrandonPanuccio <131029555+BrandonPanuccio@users.noreply.github.com>
2023-04-24 14:48:44 -0400

    Add files via upload

---
+++ startupScreen.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const StartupScreen = ({ navigation }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const navigateToScreen = (screenName) => {
+    setMenuVisible(false);
+    navigation.navigate(screenName);
+  };
+
+  return (
+
+
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Pressable onPress={toggleMenu}>
+          <Ionicons name="person-circle-outline" size={50} color="#000000" />
+        </Pressable>
+      </View>
+      <Text style={styles.logo}>Car Cat</Text>
+      {menuVisible && (
+        <View style={styles.menu}>
+          <Pressable onPress={() => navigateToScreen('My Cars')}>
+            <Text style={styles.menuItem}>My Cars</Text>
+          </Pressable>
+          <Pressable onPress={() => navigateToScreen('Add Cars')}>
+            <Text style={styles.menuItem}>Add Cars</Text>
+          </Pressable>
+          <Pressable onPress={() => navigateToScreen('Calender')}>
+            <Text style={styles.menuItem}>Calender</Text>
+          </Pressable>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    position: 'absolute',
+    top: 40,
+    right: 20,
+    marginTop: 10
+  },
+  logo: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: 'pink'
+  },
+  menu: {
+    position: 'absolute',
+    top: 70,
+    right: 20,
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 10,
+    elevation: 5,
+    zIndex: 1
+  },
+  menuItem: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: 'grey',
+    padding: 10,
+    borderRadius: 10,
+    elevation: 5,
+    zIndex: 1
+  }
+});
+
+export default StartupScreen;
