b61d1f4337dfe6f8ed9267ce291e3cea50ec0aa8
BrandonPanuccio <131029555+BrandonPanuccio@users.noreply.github.com>
2023-04-29 17:53:50 -0400

    Update startupScreen.js

--- startupScreen.js
+++ startupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Pressable} from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const StartupScreen = ({ navigation }) => {
@@ -15,13 +15,18 @@ const StartupScreen = ({ navigation }) => {
   };
 
   return (
-
-
     <View style={styles.container}>
       <View style={styles.header}>
-        <Pressable onPress={toggleMenu}>
-          <Ionicons name="person-circle-outline" size={50} color="#000000" />
-        </Pressable>
+        <View style={styles.userIcon}>
+          <Pressable onPress={toggleMenu}>
+            <Ionicons name="person-circle-outline" size={50} color="#000000" />
+          </Pressable>
+        </View>
+        <View style={styles.calendarIcon}>
+          <Pressable onPress={() => navigation.navigate('Calendar')}>
+            <Ionicons name="calendar-outline" size={50} color="#000000" />
+          </Pressable>
+        </View>
       </View>
       <Text style={styles.logo}>Car Cat</Text>
       {menuVisible && (
@@ -32,9 +37,6 @@ const StartupScreen = ({ navigation }) => {
           <Pressable onPress={() => navigateToScreen('Add Cars')}>
             <Text style={styles.menuItem}>Add Cars</Text>
           </Pressable>
-          <Pressable onPress={() => navigateToScreen('Calender')}>
-            <Text style={styles.menuItem}>Calender</Text>
-          </Pressable>
         </View>
       )}
     </View>
@@ -42,28 +44,29 @@ const StartupScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   header: {
+    flexDirection: 'column',
     position: 'absolute',
     top: 40,
     right: 20,
-    marginTop: 10
+    marginTop: 10,
+  },
+  userIcon: {
+    marginTop: 10,
+  },
+  calendarIcon: {
+    marginTop: 0,
   },
   logo: {
     fontSize: 48,
     fontWeight: 'bold',
-    color: 'pink'
+    color: 'pink',
   },
   menu: {
     position: 'absolute',
@@ -73,7 +76,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     elevation: 5,
-    zIndex: 1
+    zIndex: 1,
   },
   menuItem: {
     fontSize: 16,
@@ -83,8 +86,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     elevation: 5,
-    zIndex: 1
-  }
+    zIndex: 1,
+  },
 });
 
 export default StartupScreen;
