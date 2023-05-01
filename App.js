7d176f2943c970dc18b88b8f0cc6334698d5f262
BrandonPanuccio <131029555+BrandonPanuccio@users.noreply.github.com>
2023-04-30 23:33:57 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import StartupScreen from './startupScreen';
 import Calendar from './Calender';
 import MyCars from './MyCars';
-import AddCars from './MyCars';
+import AddCars from './AddCars';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Car Cat'>
         <Stack.Screen name="Car Cat" component={StartupScreen} />
         <Stack.Screen name="Calendar" component={Calendar} />
         <Stack.Screen name="My Cars" component={MyCars} />
-        <Stack.Screen name="Add Cars" component={AddCars} />
+        <Stack.Screen name="Add Car" component={AddCars} />
       </Stack.Navigator>
     </NavigationContainer>
   );
