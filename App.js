e119f6d0d5cd4ad6071432685eb68c1f95462ef9
BrandonPanuccio <131029555+BrandonPanuccio@users.noreply.github.com>
2023-04-30 21:34:57 -0400

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Car Cat" component={StartupScreen} />
-        <Stack.Screen name="Upcoming Appointments" component={Calendar} />
+        <Stack.Screen name="Calendar" component={Calendar} />
         <Stack.Screen name="My Cars" component={MyCars} />
         <Stack.Screen name="Add Cars" component={AddCars} />
       </Stack.Navigator>
